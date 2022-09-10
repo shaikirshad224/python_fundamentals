05b741c3c0ba30b5d3bbd1ddea48c09678a2fff6
shaikirshad224 <113006162+shaikirshad224@users.noreply.github.com>
2022-09-10 12:08:16 +0300

    Add files via upload

---
+++ D4_B63.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+introduction to list datatype:
+
+
+# In[4]:
+
+
+students=['irshad', 'mazhar', 'kumar', 'raghu', 'jhansi', 'ravi']
+
+
+# In[5]:
+
+
+print(students)
+
+
+# In[6]:
+
+
+type(students)
+
+
+# In[ ]:
+
+
+#introduction to indexing
+
+
+# In[ ]:
+
+
+# want to get kumar name in the output
+
+
+# In[7]:
+
+
+print(students[2])
+
+
+# In[8]:
+
+
+print(students[2].title())
+
+
+# In[ ]:
+
+
+#requirement to get jhansi name
+
+
+# In[11]:
+
+
+print(students[4])
+
+
+# In[12]:
+
+
+print(students[4].title())
+
+
+# In[14]:
+
+
+print(students)
+
+
+# In[ ]:
+
+
+#requirement to add David name
+
+
+# In[15]:
+
+
+students.append('david')
+
+
+# In[16]:
+
+
+print(students)
+
+
+# In[20]:
+
+
+students.insert(0,'ria')
+
+
+# In[22]:
+
+
+print(students)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
